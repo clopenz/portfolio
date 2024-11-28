2df5567529cac8e851a6d3703b1b894135ccf6dc
clopenz <clopenz@outlook.com>
2024-11-27 23:31:30 -0800

    fixed toggle animation

--- js/toggle.js
+++ js/toggle.js
@@ -1,15 +1,34 @@
+const themeToggle = document.querySelector('.theme-toggle');
+const themeToggleDiv = document.querySelector('.theme-toggle div');
+
+setTimeout(() => {
+	themeToggleDiv.style.opacity = '1';
+}, 0);
+
 if (localStorage.getItem('theme') === 'dark') {
 	if (!document.body.classList.contains('dark')) {
 		document.body.classList.add('dark');
 	}
 }
 
-const themeToggle = document.querySelector('.theme-toggle');
-const themeToggleDiv = document.querySelector('.theme-toggle div');
+if (localStorage.getItem('transition-complete') === 'true') {
+	if (document.body.classList.contains('dark')) {
+		themeToggleDiv.classList.add('complete');
+	}
+}
 
 themeToggle.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
-	themeToggleDiv.classList.toggle('complete');
+
+	if (document.body.classList.contains('dark')) {
+		setTimeout(() => {
+			themeToggleDiv.classList.add('complete');
+			localStorage.setItem('transition-complete', 'true');
+		}, 501);
+	} else {
+		themeToggleDiv.classList.remove('complete');
+		localStorage.setItem('transition-complete', 'false');
+	}
 
 	localStorage.setItem(
 		'theme',
